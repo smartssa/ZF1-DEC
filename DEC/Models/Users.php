9a661807f92ef963b09a476e96dccdf2f5b6e9f7
Darryl Clarke <darryl@artscience.ca>
2009-11-02 06:32:49 +0000

    added fetchUsernames

--- DEC/Models/Users.php
+++ DEC/Models/Users.php
@@ -7,4 +7,18 @@
 class DEC_Models_Users extends Zend_Db_Table
 {
     protected $_name = 'users';
+    
+    function fetchUsernames()
+    {
+        // return an array(userid => username);
+
+        $select  = $this->_db->select()
+        ->from($this->_name, array('key' => 'id', 'value' => 'username'));
+
+        $result = $this->getAdapter()->fetchAll($select);
+
+        sort($result);
+        return $result;
+        
+    }
 }
